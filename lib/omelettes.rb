73d711af98adb6297cac4bcc761eba749e2691e7
Mark Sim <marksim@gmail.com>
2011-07-11 23:55:59 -0500

    Remove cancan adapters.

--- lib/omelettes.rb
+++ lib/omelettes.rb
@@ -1,3 +1,1 @@
 require 'omelettes/model_additions'
-require 'omelettes/model_adapters/active_record_adapter' if defined? ActiveRecord
-require 'omelettes/model_adapters/data_mapper_adapter' if defined? DataMapper
